1ba823e2cf2051a9a68bb9b0d6f8d2004d4713f9
gaoxiaozhu <85932648+gaoxiaozhu@users.noreply.github.com>
2026-01-05 09:27:55 +0800

    update src file from pakeplus

---
+++ scripts/www/displayType-DRjpup3F.js
@@ -0,0 +1,1 @@
+import{ah as s}from"./.pnpm-B-_m2kGH.js";const i=s("displayStatus",{state:()=>({displayStatus:0,taxStatus:0}),actions:{setDisplayStatus(t){this.displayStatus=t}},persist:!0});export{i as u};
